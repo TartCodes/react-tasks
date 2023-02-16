4ba23d057e2350bd6ab7ce4ec8e4bcb97fdc1beb
TartCodes <tartcodes@gmail.com>
2023-02-16 13:43:47 -0500

    flexible hook

--- src/App.js
+++ src/App.js
@@ -5,8 +5,38 @@ import NewTask from "./components/NewTask/NewTask";
 import useHttp from "./hooks/use-http";
 
 function App() {
-	const [tasks, setTasks] = useState();
-	useHttp();
+	const [tasks, setTasks] = useState([]);
+	useHttp({
+		url: "https://react-tasks-1a2a4-default-rtdb.firebaseio.com/tasks.json"
+	});
+
+	//GET
+	const fetchTasks = async () => {
+		setIsLoading(true);
+		setError(null);
+		try {
+			const response = await fetch(
+				"https://react-tasks-1a2a4-default-rtdb.firebaseio.com/tasks.json"
+			);
+
+			if (!response.ok) {
+				throw new Error("Request failed!");
+			}
+
+			const data = await response.json();
+
+			const loadedTasks = [];
+
+			for (const taskKey in data) {
+				loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+			}
+
+			setTasks(loadedTasks);
+		} catch (err) {
+			setError(err.message || "Something went wrong!");
+		}
+		setIsLoading(false);
+	};
 
 	useEffect(() => {
 		fetchTasks();
